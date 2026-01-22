6c8c68dd7631bdbc6d07ab379b8c4ffaa24ab0a1
phucvinh0411 <phucvinh20112004@gmail.com>
2026-01-22 23:30:33 +0700

    add JWT Login

--- consumer/resource-service.js
+++ consumer/resource-service.js
@@ -1,24 +1,59 @@
+const amqp = require('amqplib'); // THÊM DÒNG NÀY VÀO ĐẦU FILE
 const express = require('express');
-const jwt=  require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const app= express();
+const app = express();
 app.use(express.json());
 
-const SERECT_KEY= "huynh_nguyen_phuc_vinh";
+const SERECT_KEY = "huynh_nguyen_phuc_vinh";
+const RABBITMQ_URL = "amqp://user:password@rabbitmq:5672";
+const QUEUE_NAME = "auth_event";
+let channel;
 
-const verifyToken= (req, res, next) => {
-    const authHeader= req.headers['authorization'];
-    const token= authHeader && authHeader.split(' ')[1];
 
-    if(!token){
-        return res.status(403).json({status: 'no token provided'});
+// Kết nối RabbitMQ
+async function connectRabbitMQ() {
+    try {
+        const connection = await amqp.connect(RABBITMQ_URL);
+        channel = await connection.createChannel();
+
+        await channel.assertQueue(QUEUE_NAME, { durable: true });
+        console.log("Consumer connected to RabbitMQ");
+
+        channel.consume(QUEUE_NAME, (msg) => {
+            if(msg!= null){
+                const content= msg.content.toString();
+                console.log("[X] receeived from RabbitMQ:", content);
+
+                const data= JSON.parse(content);
+                console.log(`Processing task for user: ${data.user ||'Unknown'}`);
+
+                channel.ack(msg); 
+            }
+        });
+
+    } catch (error) {
+        console.error("Failed to connect to RabbitMQ", error);
+        setTimeout(connectRabbitMQ, 3000); // Thử kết nối lại sau 3 giây
+    }
+}
+
+connectRabbitMQ();
+
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({ status: 'no token provided' });
     }
 
-    jwt.verify(token, SERECT_KEY, (err, decodedData)=>{
-        if(err){
-            return res.status(401).json({status: 'unauthorized'});
+    jwt.verify(token, SERECT_KEY, (err, decodedData) => {
+        if (err) {
+            return res.status(401).json({ status: 'unauthorized' });
         }
-        req.user= decodedData;
+        req.user = decodedData;
         next();
     });
 };
@@ -26,9 +61,9 @@ const verifyToken= (req, res, next) => {
 app.get('/api/profile', verifyToken, (req, res) => {
     console.log("Access for: ", req.user.fullName);
     res.json({
-        message:"Verify token successfully",
+        message: "Verify token successfully",
         data: req.user
     });
 });
 
-app.listen(3000, ()=>{console.log("Resource Service (Consumer) running on port 3000")});
+app.listen(3000, () => { console.log("Resource Service (Consumer) running on port 3000") });
--- producer/auth-service.js
+++ producer/auth-service.js
@@ -1,10 +1,31 @@
+const amqp = require('amqplib');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const app= express();
 app.use(express.json());
 
-const SERECT_KEY= "huynh_nguyen_phuc_vinh";
+const SERECT_KEY = "huynh_nguyen_phuc_vinh";
+const RABBITMQ_URL = "amqp://user:password@rabbitmq:5672";
+const QUEUE_NAME = "auth_event";
+let channel;
+
+
+// Kết nối RabbitMQ
+async function connectRabbitMQ() {
+    try {
+        const connection = await amqp.connect(RABBITMQ_URL);
+        channel = await connection.createChannel();
+
+        await channel.assertQueue(QUEUE_NAME, { durable: true });
+        console.log("Consumer connected to RabbitMQ");
+    } catch (error) {
+        console.error("Failed to connect to RabbitMQ", error);
+        setTimeout(connectRabbitMQ, 3000); // Thử kết nối lại sau 3 giây
+    }
+}
+
+connectRabbitMQ();
 
 app.post('/login', (req, res) => {
     const {username, password}= req.body;
@@ -17,12 +38,20 @@ app.post('/login', (req, res) => {
         };
         const token= jwt.sign(userPayLoad, SERECT_KEY, {expiresIn: '20m'});
 
+        const message = JSON.stringify({event: 'USER_LOGIN', user: userPayLoad.fullName, timestamp: new Date()});
+
+        channel.sendToQueue(QUEUE_NAME, Buffer.from(message), {persistent: true});
+        
         console.log("Token for: ", userPayLoad.fullName);
         return res.json({
             status: 'success',
             token: token
         });
     }
+
+    // authEvent.status = "FAILED";
+    channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify({user: username, timestamp: new Date(), event: 'USER_LOGIN_FAILED'})), {persistent: true});
+
     res.status(401).json({status: 'unauthorized'});
 });
 
